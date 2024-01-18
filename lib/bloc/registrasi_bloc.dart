ff85c26e806374cbf1f7034a39d1be7192ee3066
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-01-18 16:37:51 +0700

    tiketapp,login regist fix

---
+++ lib/bloc/registrasi_bloc.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:tiketapp/helpers/api.dart';
+import 'package:tiketapp/helpers/api_url.dart';
+import 'package:tiketapp/model/registrasi.dart';
+
+class RegistrasiBloc {
+  static Future<Registrasi> registrasi(
+      {String? nama, String? email, String? password}) async {
+    String apiUrl = ApiUrl.registrasi;
+
+    var body = {"nama": nama, "email": email, "password": password};
+
+    var response = await Api().post(apiUrl, body);
+    var jsonObj = json.decode(response.body);
+    return Registrasi.fromJson(jsonObj);
+  }
+}
