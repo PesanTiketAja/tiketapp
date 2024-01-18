a49c9f2020652038923bd2d3d3393d2928a09a26
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-01-18 12:49:32 +0700

    push

--- android/app/src/main/kotlin/com/example/keuanganpribadi/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.keuanganpribadi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/bloc/logout_bloc.dart
+++
@@ -1,7 +0,0 @@
-import 'package:tiket/helpers/user_info.dart';
-
-class LogoutBloc {
-  static Future logout() async {
-    await UserInfo().logout();
-  }
-}
